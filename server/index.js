eacd83cf25e45f101c70985801e29f5a53718d33
Usman Cheema <140833929+Usman-cheema79@users.noreply.github.com>
2024-08-07 03:20:13 +0500

    cors

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ app.use(express.json());
 app.use(cors({ origin: '*' }));
 
 // Define routes
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello from serverless function!' });
+  });
+  
 app.use('/api', userRoutes);
 app.use('/api', productRoutes);
 
