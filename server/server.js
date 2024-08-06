8d9dd4c3ac7c0f3b1e3fb71c84ba2afe12128a64
umarfarooq036 <f2020266036@umt.edu.pk>
2024-08-07 01:47:35 +0500

    first servere

--- server/server.js
+++ server/server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const userRoutes = require('./routes/userRoutes');
-const productRoutes = require('./routes/productRoutes');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoute');
+const productRoutes = require('./routes/productRoute');
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+// Middleware
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000' }));
+
+// Define routes
 app.use('/api', userRoutes);
 app.use('/api', productRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
