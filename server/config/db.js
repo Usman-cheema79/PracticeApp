f2d4de5c8c96d1123a90a0f86451475e18483525
Usman Cheema <arshadcheema1973@gmail.com>
2024-10-31 23:03:23 +0500

    a

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URI, {
+    await mongoose.connect("mongodb+srv://abcd:abcd@cluster0.dqllq11.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
